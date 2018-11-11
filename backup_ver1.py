19e2f3ddba9b3642383668c9f1326e57e5c62872
Gray.Z <zrg523@gmail.com>
2018-11-11 22:24:35 +0800

    update

---
+++ backup_ver1.py
@@ -0,0 +1,25 @@
+import os
+import time
+
+source = ['D:\\data\\logs']
+
+target_dir = 'D:\\data\\logs\\backup'
+
+target = target_dir + os.sep + \
+         time.strftime('%Y%m%d%H%M%S') + '.zip'
+
+# 如果目标目录还不存在，则进行创建
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)  # 创建目录
+
+# 5. 我们使用 zip 命令将文件打包成 zip 格式
+zip_command = 'zip -r {0} {1}'.format(target, ' '.join(source))
+
+# 运行备份
+print('Zip command is:')
+print(zip_command)
+print('Running:')
+if os.system(zip_command) == 0:
+    print('Successful backup to', target)
+else:
+    print('Backup FAILED')
