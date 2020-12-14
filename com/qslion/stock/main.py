bfe05f774b779c5ddaefd75e3117f14b6c3857c2
gray <gray@google.com>
2020-12-14 17:49:15 +0800

    update commit

--- com/qslion/stock/main.py
+++ com/qslion/stock/main.py
@@ -11,6 +11,7 @@ import pandas as pd
 
 
 def init(context):
+    unsubscribe(symbols='*', frequency='60s')
     # schedule 定时任务详见： https://www.myquant.cn/docs/python/python_basic#7bce6621a1abafe8
     # 每月第一个交易日的09:40 定时执行algo任务
     schedule(schedule_func=algo, date_rule='1d', time_rule='09:40:00')
@@ -19,125 +20,76 @@ def init(context):
     # 最大持有股票数
     context.hold_max = 2
     # 持有天数
-    context.periods = 3
-    # 当前持仓数
-    context.hold_count = 0
-    # 各股票持仓时间
-    context.hold_days = {}
+    context.periods = 2
     # 最大交易资金比例
     context.ratio = 0.8
+    # 标的池
+    context.stocks = pd.DataFrame()
 
 
 def algo(context):
     # 获取当前时间
     now = context.now
+    print("当前执行日期为：", context.now.strftime("%Y-%m-%d %H:%M:%S"))
     # 获取上一个交易日
     last_day = get_previous_trading_date(exchange='SHSE', date=now)
-    # print("上一个交易日:", last_day)
-    data_df = get_filter_stocks(context, last_day)
-    print(data_df)
-    data_df.to_csv('d:\\his1.csv', encoding='utf_8_sig')
-    context.stocks = data_df
-    unsubscribe(symbols='*', frequency='60s')
-    subscribe(symbols=','.join(data_df['symbol']), frequency='60s')
+    if not context.stocks.empty:
+        account_symbols = list(map(lambda x: x.symbol, context.account().positions()))
+        unsub_stocks = context.stocks[~context.stocks['symbol'].isin(account_symbols)]
+        if not unsub_stocks.empty:
+            # 取消未持仓订阅，持仓订阅在卖出后取消
+            unsubscribe(symbols=','.join(unsub_stocks['symbol']), frequency='60s')
 
-    for stock in context.hold_days:
-        # 持仓天数增加
-        context.hold_days[stock] += 1
-        # 持仓到期股票
-        if context.hold_days[stock] > context.periods:
-            order_target_percent(symbol=stock, percent=0, order_type=OrderType_Market,
-                                 position_side=PositionSide_Long)
+    data_df = get_filter_stocks(context, last_day)
+    # print(data_df)
+    if not data_df.empty:
+        # data_df.to_csv('d:\\his1.csv', encoding='utf_8_sig')
+        context.stocks = data_df
+        subscribe(symbols=','.join(data_df['symbol']), frequency='60s')
 
 
 def on_bar(context, bars):
-    # 持仓数未达上限
-    if context.hold_count < context.hold_max:
-        # 打印bar数据
+    # 买入策略：持仓数未达上限
+    if len(context.account().positions()) < context.hold_max:
         stock_df = context.stocks[context.stocks['symbol'] == bars[0]['symbol']]
-        if bars[0]['close'] < stock_df.iloc[0].ma5 * 1.02 and not context.account().position(symbol=bars[0]['symbol'],
-                                                                                             side=PositionSide_Long):
-            print("bars:", bars[0]['close'])
-            print("买入信号====>>", stock_df.iloc[0].ma5 * 1.02)
+        if not stock_df.empty and bars[0]['close'] < stock_df.iloc[0].ma5 * 1.02 and not context.account().position(
+                symbol=bars[0]['symbol'], side=PositionSide_Long):
             # 计算每个个股应该在持仓中的权重
             percent = 1.0 / context.hold_max * context.ratio
             # 限价买入
             order_target_percent(symbol=bars[0]['symbol'], percent=percent, order_type=OrderType_Limit,
                                  price=bars[0]['close'], position_side=PositionSide_Long)
-            context.hold_count = context.hold_count + 1
-            context.hold_days[bars[0]['symbol']] = 0
-    else:
-        print("持仓数达上限:", context.hold_count)
+            print("标的：{},买入信号：{},买入限价：{},时间：{},持仓量:{}".format(bars[0]['symbol'], stock_df.iloc[0].ma5 * 1.02,
+                                                              bars[0]['close'],
+                                                              bars[0]['eob'].strftime("%Y-%m-%d %H:%M:%S"),
+                                                              len(context.account().positions())))
 
-        # 4.获得卖出股票池
+    if len(context.account().positions()) > 0 and context.account().position(symbol=bars[0]['symbol'],
+                                                                             side=PositionSide_Long):
+        buy_date = context.account().position(symbol=bars[0]['symbol'], side=PositionSide_Long).created_at.date()
+        if (context.now.date() - buy_date).days >= 1:
+            vwap = context.account().position(symbol=bars[0]['symbol'], side=PositionSide_Long).vwap
+            cur_close = bars[0]['close']
+            # 卖出策略1：预期收益8%
+            if cur_close / vwap > 1.1:
+                order_target_percent(symbol=bars[0]['symbol'], percent=0, order_type=OrderType_Market,
+                                     position_side=PositionSide_Long)
 
+                unsubscribe(symbols=bars[0]['symbol'], frequency='60s')
+                print("标的：{},买出信号（1.08）：{},持仓均价：{},时间：{},持仓量:{}".format(bars[0]['symbol'], cur_close,
+                                                                        vwap,
+                                                                        bars[0]['eob'].strftime("%Y-%m-%d %H:%M:%S"),
+                                                                        len(context.account().positions())))
 
-def stocks_to_sell(context):
-    sell_stocks = []
-    # 持仓到期股票
-    for stock in context.hold_days:
-        if context.hold_days[stock] > context.periods:
-            sell_stocks.append(stock)
-    # 更新当前持仓数
-    context.hold_count = len(context.account().positions()) - len(sell_stocks)
-    return sell_stocks
-
-
-# 8.交易操作
-def trade_stocks(sell_stocks, buy_stocks, context):
-    # 卖出操作
-    for stock in sell_stocks:
-        order_target_value(stock, 0)
-        log.info(str(stock) + ":买出操作")
-    # 每股资金
-    Count = max([1, len(buy_stocks)])
-    one_cash = context.account().cash / Count
-
-    # 买入操作
-    for stock in buy_stocks:
-        order_target_percent(symbol=stock, percent=0, order_type=OrderType_Market,
-                             position_side=PositionSide_Long)
-        log.info(str(stock) + ":买入操作，买入金额：" + str(one_cash))
-        context.hold_days[stock] = 0
-
-
-# code.to_csv('d:\\code.csv')
-
-# 通过get_instruments获取所有的上市股票代码 详见：https://www.myquant.cn/docs/python/python_select_api#8ba2064987fb1d1f
-# code = get_instruments(exchanges=['SHSE', 'SZSE'], sec_types=SEC_TYPE_STOCK, df=True)
-
-# 获取所有股票在这个时候的市值， 详见： https://www.myquant.cn/docs/python/python_select_api#8ba2064987fb1d1f
-# fundamental = get_fundamentals_n('trading_derivative_indicator', code['symbol'].to_list(),
-#                                  context.now, fields='TOTMKTCAP', order_by='TOTMKTCAP', count=1, df=True)
-
-# 去掉数据中包含‘SHSE.900’ 和 ‘SZSE.200’的股票，即B股。
-# fundamental = fundamental.loc[~fundamental['symbol'].str.contains('SHSE.900'), :]
-# fundamental = fundamental.loc[~fundamental['symbol'].str.contains('SZSE.200'), :]
-
-# 对市值进行排序，并且获取前30个。 最后将这个series 转化成为一个list即为标的池
-# trade_symbols = fundamental.reset_index(drop=True).loc[:29, 'symbol'].to_list()
-
-# print('本次股票池有股票数目: ', len(trade_symbols))
-
-# # 计算每个个股应该在持仓中的权重
-# percent = 1.0 / len(trade_symbols) * context.ratio
-#
-# # 获取当前所有仓位 详见 https://www.myquant.cn/docs/python/python_concept#8079e2e4dad05879
-# positions = context.account().positions()
-#
-# # 平不在标的池的仓位
-# for position in positions:
-#     symbol = position['symbol']
-#     if symbol not in trade_symbols:
-#         order_target_percent(symbol=symbol, percent=0, order_type=OrderType_Market,
-#                              position_side=PositionSide_Long)
-#         print('市价单平不在标的池的', symbol)
-#
-# # 将标中已有持仓的和还没有持仓的都调整到计算出来的比例。
-# for symbol in trade_symbols:
-#     order_target_percent(symbol=symbol, percent=percent, order_type=OrderType_Market,
-#                          position_side=PositionSide_Long)
-#     print(symbol, '以市价单调整至权重', percent)
+        # 卖出策略2：持仓到期股票
+        if (context.now.date() - buy_date).days >= context.periods:
+            order_target_percent(symbol=bars[0]['symbol'], percent=0, order_type=OrderType_Market,
+                                 position_side=PositionSide_Long)
+            unsubscribe(symbols=bars[0]['symbol'], frequency='60s')
+            print("标的：{},买出信号（periods）：{},持仓均价：{},时间：{},持仓量:{}".format(bars[0]['symbol'], cur_close,
+                                                                       vwap,
+                                                                       bars[0]['eob'].strftime("%Y-%m-%d %H:%M:%S"),
+                                                                       len(context.account().positions())))
 
 
 def get_filter_stocks(context, history_day):
@@ -161,8 +113,11 @@ def get_filter_stocks(context, history_day):
     history_df['amplitude'] = history_df.apply(lambda x: (x.high - x.low) / x.low, axis=1).astype(float)
 
     # 过滤:振幅>8%,向下振幅>=5%,向上振幅大>%2,涨幅>=%5
-    history_df = history_df.loc[lambda x: (x.amplitude >= 0.08) & ((x.close - x.low) / x.low >= 0.05) & (
-            (x.high - x.close) / x.close > 0.02) & (x.close > x.pre_close)]
+    history_df = history_df.loc[lambda x: (x.amplitude >= 0.08) &  # 振幅>8%
+                                          ((x.open - x.low) / x.low >= 0.03) &  # 向下振幅>=3%
+                                          ((x.high - x.close) / x.close > 0.02) &  # 向上振幅大>%2
+                                          ((x.close - x.pre_close) / x.pre_close >= 0.05) &  # 涨幅>=%5
+                                          (x.close > x.pre_close)]  # 收盘高于昨日
 
     ## 排序 ##
     history_df.sort_index(axis=1)
@@ -186,12 +141,13 @@ def get_filter_stocks(context, history_day):
                                    fields='symbol, open, close, low, high, eob', adjust=ADJUST_PREV, df=True)
         # 收盘价在5日均线之上
         ma5 = history_n_data['close'].mean()
+        max_high3 = history_n_data['high'][1:4].max()
         if row.close > ma5:
             pre_row_df = pre_his_df[pre_his_df.symbol == row.symbol]
-            if not pre_his_df.empty:
+            if not pre_row_df.empty:
                 pre_row = pre_row_df.iloc[0]
-                # 当日最低价小于等于上日最低价(@todo 待优化)，当日收盘价高于上日最高价,上一日涨幅或跌幅<9%
-                if row.low <= pre_row.low and row.close > pre_row.high and abs(
+                # 当日最低价小于等于上日最低价的80%，当日收盘价高于昨日最高价,最高价高于3日最高价，上一日涨幅或跌幅<9%
+                if row.low <= pre_row.low * 1.02 and row.close > pre_row.high and row.high > max_high3 and abs(
                         (pre_row.close - pre_row.pre_close) / pre_row.close) < 0.09:
                     row['anme'] = all_stock[all_stock.symbol == row.symbol].iloc[0].sec_name
                     row['ma5'] = ma5
@@ -218,9 +174,9 @@ if __name__ == '__main__':
         filename='main.py',
         mode=MODE_BACKTEST,
         token='b526e92627f493aa90cdbae30a75407b63d1eae2',
-        backtest_start_time='2020-12-08 08:00:00',
-        backtest_end_time='2020-12-08 16:00:00',
+        backtest_start_time='2020-10-09 09:30:00',
+        backtest_end_time='2020-10-23 16:00:00',
         backtest_adjust=ADJUST_PREV,
-        backtest_initial_cash=1000000,
+        backtest_initial_cash=100000,
         backtest_commission_ratio=0.0001,
         backtest_slippage_ratio=0.0001)
